43e6b26283cfd4f5a7ad916d1cde768dd2b5de31
DESKTOP-0B1CHHP\phamhuudat <phamhuudatk58@gmail.com>
2018-05-23 01:28:02 +0700

    chinh id game tv

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ var url = require('./DemoUrl.js');
 //UCCIYZ5cZ0V6Mx_DRGsRhzeA - GametV
 // x`
 var ACCESS_TOKEN = 'AIzaSyAOtboeuzSHUJA8Uq8vB2vxQQO24yyloNo';
-var urlPesTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UC8QbPivxASef0O1rCmCzxYg&key="+ACCESS_TOKEN;
+var urlPesTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UC7LVqMkj1e66ZzRo9SPvHvg&key="+ACCESS_TOKEN;
 var urlLienMinhAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCHKuLpFy9q8XDp0i9WNHkDw&key="+ACCESS_TOKEN;
 var urlLienQuanGameTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCl5QoHQVENUl7ZyPQlC3CaA&key="+ACCESS_TOKEN;
 var urlLienQuanAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCpnQwjzvDm1MOMtZV2zkVpA&key="+ACCESS_TOKEN;
-var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UC9xqfxrfffeuVxXKhUZaeUA&key="+ACCESS_TOKEN;
+var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCCIYZ5cZ0V6Mx_DRGsRhzeA&key="+ACCESS_TOKEN;
 
 //Thêm phần chức năng
  function CheckNotification() {
@@ -33,4 +33,4 @@ var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&orde
     	url.GetObjectYoutube(urlPesTv,'icon');
     });
 }
-setInterval(CheckNotification,30000);
+setInterval(CheckNotification,40000);

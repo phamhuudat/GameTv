6cf63c8810c1f386cfcee5f117e5b91225d84139
DESKTOP-0B1CHHP\phamhuudat <phamhuudatk58@gmail.com>
2018-05-23 00:46:00 +0700

    Server add db

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var urlPesTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=da
 var urlLienMinhAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCHKuLpFy9q8XDp0i9WNHkDw&key="+ACCESS_TOKEN;
 var urlLienQuanGameTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCl5QoHQVENUl7ZyPQlC3CaA&key="+ACCESS_TOKEN;
 var urlLienQuanAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCpnQwjzvDm1MOMtZV2zkVpA&key="+ACCESS_TOKEN;
-var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCTSkEZ84nY5gBjiXekhkgmQ&key="+ACCESS_TOKEN;
+var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UC9xqfxrfffeuVxXKhUZaeUA&key="+ACCESS_TOKEN;
 
 //Thêm phần chức năng
  function CheckNotification() {

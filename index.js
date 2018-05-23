534464cd75d16a3c84f3364313d6a37018c96370
DESKTOP-0B1CHHP\phamhuudat <phamhuudatk58@gmail.com>
2018-05-23 21:24:37 +0700

    đổi id gametv

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var url = require('./DemoUrl.js');
 var ACCESS_TOKEN = 'AIzaSyAOtboeuzSHUJA8Uq8vB2vxQQO24yyloNo';
 var urlPesTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UC7LVqMkj1e66ZzRo9SPvHvg&key="+ACCESS_TOKEN;
 var urlLienMinhAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCHKuLpFy9q8XDp0i9WNHkDw&key="+ACCESS_TOKEN;
-var urlLienQuanGameTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCl5QoHQVENUl7ZyPQlC3CaA&key="+ACCESS_TOKEN;
+var urlLienQuanGameTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCCIYZ5cZ0V6Mx_DRGsRhzeA&key="+ACCESS_TOKEN;
 var urlLienQuanAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCpnQwjzvDm1MOMtZV2zkVpA&key="+ACCESS_TOKEN;
 var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCwq95KcdMM5BRMwYsJX4iHg&key="+ACCESS_TOKEN;
 

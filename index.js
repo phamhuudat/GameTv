60d5806dd2ed8be17e34833c678745f13ea9aea5
DESKTOP-0B1CHHP\phamhuudat <phamhuudatk58@gmail.com>
2018-05-23 17:14:24 +0700

    đổi id gametv

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ app.get("/",function(req, res){
 var events = require('events');
 //var eventEmitter = new events.EventEmitter();
 var url = require('./DemoUrl.js');
-//UCCIYZ5cZ0V6Mx_DRGsRhzeA - GametV
+//UCwq95KcdMM5BRMwYsJX4iHg - GametV
+//UCTSkEZ84nY5gBjiXekhkgmQ - VECTV
 // x`
 var ACCESS_TOKEN = 'AIzaSyAOtboeuzSHUJA8Uq8vB2vxQQO24yyloNo';
 var urlPesTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UC7LVqMkj1e66ZzRo9SPvHvg&key="+ACCESS_TOKEN;
 var urlLienMinhAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCHKuLpFy9q8XDp0i9WNHkDw&key="+ACCESS_TOKEN;
 var urlLienQuanGameTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCl5QoHQVENUl7ZyPQlC3CaA&key="+ACCESS_TOKEN;
 var urlLienQuanAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCpnQwjzvDm1MOMtZV2zkVpA&key="+ACCESS_TOKEN;
-var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCwq95KcdMM5BRMwYsJX4iHg&key="+ACCESS_TOKEN;
+var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCTSkEZ84nY5gBjiXekhkgmQ&key="+ACCESS_TOKEN;
 
 //Thêm phần chức năng
  function CheckNotification() {

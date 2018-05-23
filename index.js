26270b35adb987d181a2c034ff86e9fc86bba342
DESKTOP-0B1CHHP\phamhuudat <phamhuudatk58@gmail.com>
2018-05-23 09:18:56 +0700

    đổi id game tv

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var urlPesTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=da
 var urlLienMinhAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCHKuLpFy9q8XDp0i9WNHkDw&key="+ACCESS_TOKEN;
 var urlLienQuanGameTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCl5QoHQVENUl7ZyPQlC3CaA&key="+ACCESS_TOKEN;
 var urlLienQuanAgenna="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCpnQwjzvDm1MOMtZV2zkVpA&key="+ACCESS_TOKEN;
-var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCwq95KcdMM5BRMwYsJX4iHg&key="+ACCESS_TOKEN;
+var urlAoeGamtTv="https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&type=video&eventType=live&maxResults=50&channelId=UCj9o8NV6zGzc4knUARRKdcw&key="+ACCESS_TOKEN;
 
 //Thêm phần chức năng
  function CheckNotification() {
